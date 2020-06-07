7a1ec49602c7ea585abb2d041c831adca6115554
zmh <11@qq.com>
2020-06-07 11:12:31 +0800

    config/autoload/dependencies.php的使用参考

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -9,6 +9,6 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
-
+//用法参考 https://doc.hyperf.io/#/zh-cn/di?id=%E6%8A%BD%E8%B1%A1%E5%AF%B9%E8%B1%A1%E6%B3%A8%E5%85%A5
 return [
 ];
